b40ea45dd345b20d8f0c8750f519ef1b9603a60f
baob <coder@onesandthrees.com>
2020-05-03 17:29:42 +0100

    WORKING hello world server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const http = require('http');
 const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const dotenv = require('dotenv');
 const config = require('./config');
 
@@ -8,8 +10,13 @@ const port = config.port() || 3000;
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.post('/', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
 });
 
 app.listen(port, function () {
